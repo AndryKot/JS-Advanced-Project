c48ab5a9addb184ca901c22d83d55ddb5e37c220
AndryKot <andry.kot.9@gmail.com>
2019-10-06 22:15:00 +0300

    change my project style and start to code

---
+++ js/movie.js
@@ -0,0 +1,34 @@
+var options= {
+    url: 'youtube.com',
+    title: 'film',
+    image: 'image.com',
+    description: 'films of the film',
+    actors: 'Big Small, Thin Tall, Blam Blam',
+    contry: 'Ukraine',
+    year: 2020
+};
+
+function Movie(options) {
+    this.url = options.url;
+    this.title = options.title;
+    this.image = options.image;
+    this.description = options.description;
+    this.actors = options.actors;
+    this.contry = options.contry;
+    this.year = options.year;
+
+    this.getMainInformation = function () {
+        console.log(this.title +' '+ this.year +' '+ this.description)
+    };
+    this.checkDateFilm = function () {
+        if(this.year <=2010){
+            console.log("Film old");
+        }else{
+            console.log("Film new");
+        }
+    }
+}
+
+var film = new Movie(options);
+film.getMainInformation();
+film.checkDateFilm();
