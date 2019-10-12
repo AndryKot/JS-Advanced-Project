c0146c55878c920e4de615534d5ee44a7cc6f52c
AndryKot <andry.kot.9@gmail.com>
2019-10-12 07:15:59 +0300

    Change Movie, MovieList, sort and add some methods

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-var options= {
-    ID: '',
+var option1 =
+    {
+    ID: '1001',
     shortCode: '',
     title: 'Joker',
     rating: 8.1,
@@ -11,61 +12,114 @@ var options= {
     producerName: 'Todd Phillips',
     actorsList: [],
     trailerUrl: 'https://www.youtube.com/embed/t433PEQGErc'
-};
+    };
+var option2 ={
+        ID: '1002',
+        shortCode: '',
+        title: 'Spider-Man: Far from Home',
+        rating: 7.6,
+        releaseYear: 2019,
+        country: ['USA'],
+        translation: 'origin',
+        duration: 129,
+        synopsis: 'Peter Parker and his friends go on a summer trip to Europe. However, they will hardly be able to rest - Peter will have to agree to help Nick Fury uncover the mystery of creatures that cause natural disasters and destruction throughout the continent.',
+        producerName: 'Jon Watts',
+        actorsList: ['Tom Holland', 'Jake Gyllenhaal', 'Zendaya'],
+        trailerUrl: 'https://www.youtube.com/embed/DYYtuKyMtY8'
+    };
+var option4 ={
+        ID: '1004',
+        shortCode: '',
+        title: 'It Chapter Two',
+        rating: 7.0,
+        releaseYear: 2019,
+        country: ['USA'],
+        translation: 'origin',
+        duration: 169,
+        synopsis: '27 years after overcoming the malevolent supernatural entity Pennywise, the former members of the Losers\' Club, who have grown up and moved away from Derry, are brought back together by a devastating phone call',
+        producerName: 'Andy Muschietti',
+        actorsList: ['James McAvoy', 'Jessica Chastain', 'Bill Hader'],
+        trailerUrl: 'https://www.youtube.com/embed/zqUopiAYdRg'
+    };
+var option3 ={
+        ID: '1003',
+        shortCode: '',
+        title: 'The Old Man & the Gun ',
+        rating: 7.6,
+        releaseYear: 2018,
+        country: ['USA'],
+        translation: 'origin',
+        duration: 93,
+        synopsis: 'The true story of Forrest Tucker, from his audacious escape from San Quentin at the age of 70 to an unprecedented string of heists that confounded authorities and enchanted the public. Wrapped up in the pursuit are a detective, who becomes captivated with Forrest’s commitment to his craft, and a woman, who loves him in spite of his chosen profession.',
+        producerName: 'David Lowery',
+        actorsList: ['Robert Redford', 'Sissy Spacek', 'Casey Affleck'],
+        trailerUrl: 'https://www.youtube.com/embed/d7rlUe-Thvk'
+    };
+var option5 ={
+        ID: '1005',
+        shortCode: '',
+        title: 'The Lion King',
+        rating: 7.1,
+        releaseYear: 2019,
+        country: ['USA'],
+        translation: 'origin',
+        duration: 118,
+        synopsis: 'Simba idolises his father, King Mufasa, and takes to heart his own royal destiny. But not everyone in the kingdom celebrates the new cub\'s arrival. Scar, Mufasa\'s brother—and former heir to the throne—has plans of his own. The battle for Pride Rock is ravaged with betrayal, tragedy and drama, ultimately resulting in Simba\'s exile. With help from a curious pair of newfound friends, Simba will have to figure out how to grow up and take back what is rightfully his.',
+        producerName: 'Jon Favreau',
+        actorsList: ['Donald Glover', 'Beyoncé Knowles', 'James Earl Jones'],
+        trailerUrl: 'https://www.youtube.com/embed/4CbLXeGSDxg'
+    };
 
-function Movie(options) {
-    this.title = options.title;
-    this.rating = options.rating;
-    this.releaseYear = options.releaseYear;
-    this.country = options.country;
-    this.translation = options.translation;
-    this.duration = options.duration;
-    this.synopsis = options.synopsis;
-    this.producerName = options.producerName;
-    this.actorsList = options.actorsList;
-    this.trailerUrl = options.trailerUrl;
 
+function Movie(option) {
+    this.id = option.id;
+    this.title = option.title;
+    this.rating = option.rating;
+    this.releaseYear = option.releaseYear;
+    this.country = option.country;
+    this.translation = option.translation;
+    this.duration = optios.duration;
+    this.synopsis = option.synopsis;
+    this.producerName = option.producerName;
+    this.actorsList = option.actorsList;
+    this.trailerUrl = option.trailerUrl;
 
-    this.getTitle = function() {
+}
+Movie.prototype ={
+    getId: function(){
+        return this.id;
+    },
+    getTitle: function() {
         return this.title;
-    }
-
-
-    this.getRating = function() {
+    },
+   getRating: function() {
         return this.rating;
-    }
-
-    this.getReleaseYear = function() {
+    },
+    getReleaseYear: function() {
         return this.releaseYear;
-    }
-
-    this.getCountry = function() {
+    },
+    getCountry: function() {
         return this.country;
-    }
-
-    this.getTranslation = function() {
+    },
+    getTranslation: function() {
         return this.translation;
-    }
-
-    this.getDuration = function() {
+    },
+    getDuration: function() {
         return this.duration;
-    }
-
-    this.getSynopsis = function() {
+    },
+   getSynopsis: function() {
         return this.synopsis;
-    }
-
-    this.getProducerName = function() {
+    },
+   getProducerName: function() {
         return this.producerName;
-    }
-
-    this.getActorsList = function() {
+    },
+   getActorsList: function() {
         return this.actorsList;
-    }
-    this.getTrailerUrl = function () {
+    },
+    getTrailerUrl: function () {
         return this.trailerUrl;
     }
-}
+};
 
 
 
@@ -92,63 +146,60 @@ function Movie(options) {
 
 
 
-function MovieList(data) {
-    this.data = data;
-
-    this.searchMovieByTitle = function() {
-
-    }
-
-    this.sortByTitle = function() {
-
-    }
-
-    this.sortByReleaseYear = function() {
-
-    }
-
-    this.sortByRating = function() {
-
-    }
-
-
-    this.getByPopular = function() {
-
-    }
-
-    this.getPopularMovies = function() {
-
-    }
-
-    this.getUncomingMovies = function() {
-
-    }
-
-    this.getTopRatedMovies = function() {
-
-    }
+function MovieList(options) {
+    this.options = options;
 }
+MovieList.prototype = {
+    searchMovieByTitle: function() {
+        console.log("smart List Movies");
+    },
+    sortByTitle: function() {
+        return this.options.sort((a, b) => a.title > b.title ? 1 : -1);
+    },
+    sortByReleaseYear: function() {
+        return this.options.sort((a, b) => a.releaseYear < b.releaseYear ? 1 : -1);
+    },
+    sortByRating: function() {
+        return this.options.sort((a, b) => a.rating < b.rating ? 1 : -1);
+    },
+    getByPopular: function() {
+    },
+    getPopularMovies: function() {
+    },
+    getUncomingMovies: function() {
+    },
+    getTopRatedMovies: function() {
+    },
+    deleteMovie: function(index, quantity) {
+        return list.splice(index, quantity);
+        return this.options.splice(index, quantity);
+    },
+
+    addMovie: function(movie) {
+        return list.push(movie);
+        return this.options.push(movie);
+    }
+};
 
 
+var movieList = new MovieList([option1, option2, option3, option4, option5]);
 
-var movieList = new MovieList();
-
-var film = new Movie(options);
-
-console.log(film);
-
-movieList.searchMovieByTitle();
-
-movieList.sortByTitle();
-
-movieList.sortByReleaseYear();
-
-movieList.sortByRating();
-
-movieList.getByPopular();
-
-movieList.getPopularMovies();
-
-movieList.getUncomingMovies();
-
-movieList.getTopRatedMovies();
+// var film = new Movie(options);
+//
+// console.log(film);
+// console.log(film.getTrailerUrl());
+console.log("============");
+console.log(movieList.sortByTitle());;
+console.log("============");
+console.log("############");
+console.log(movieList.sortByReleaseYear());;
+console.log("############");
+console.log(movieList.sortByRating());;
+//
+// movieList.getByPopular();
+//
+// movieList.getPopularMovies();
+//
+// movieList.getUncomingMovies();
+//
+// movieList.getTopRatedMovies();
